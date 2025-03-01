518f059194118f4e39ccc3c430b086b0786294c0
Abhay Kumar <120698096+ABHAYKUMAR0208@users.noreply.github.com>
2025-03-01 09:53:54 +0530

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -36,52 +36,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet()); // Security Headers
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
 app.use(compression()); // Gzip Compression
 app.use(cookieParser());
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: *,
-    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
-app.use(
-  cors({
-    origin: process.env.CLIENT_BASE_URL,
-    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
 app.use(
   cors({
     origin: process.env.CLIENT_BASE_URL,
@@ -132,4 +89,4 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
